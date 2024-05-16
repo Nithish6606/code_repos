1448334aeac657b5b09c0047323cf7b58464ee96
Nithish <madanithish6606@gmail.com>
2024-05-16 14:40:54 +0530

    Modified greet

--- gre.py
+++ gre.py
@@ -1,3 +1,12 @@
+def log_function_call(func):
+    def wrapper(*args, **kwargs):
+        print(f"Calling {func.__name__} with arguments {args}, {kwargs}")
+        result = func(*args, **kwargs)
+        print(f"{func.__name__} returned {result}")
+        return result
+    return wrapper
+
+@log_function_call
 def greet(name:str)->str:
     return f"Hello, {name}!"
 def main()->None:
